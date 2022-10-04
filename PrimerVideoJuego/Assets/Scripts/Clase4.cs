12c865a8ab6536a2bb8f8645678c8a45bbfa8409
javiertruquin <javiertruquin@gmail.com>
2022-10-03 21:18:26 -0300

    Probando un comitin
    comitin tintin

--- PrimerVideoJuego/Assets/Scripts/Clase4.cs
+++ PrimerVideoJuego/Assets/Scripts/Clase4.cs
@@ -5,57 +5,18 @@ using UnityEngine;
 public class Clase4 : MonoBehaviour
 {
 
-    // public Enemigo objeto;
-    // public float vidaPersonaje=100;
-    // int money=0;
-    // public float vidaJugador=100;
     // Start is called before the first frame update
     void Start()
     {
-        // int numeritoA = 30;
-        // SumarCinco(ref numeritoA);
-        // Debug.Log(numeritoA);
-        // objeto = GameObject.Find("Pared");
-        // objeto = FindObjectOfType<Enemigo>();
-        // objeto.DestruirElMundo();
+
     }
 
 
     // Update is called once per frame
     void Update()
     {
-        // Debug.DrawRay(transform.position,new Vector3(1,0,0)*5,Color.red);
-        // JuntarDinero();
-        // QuitarVidaJugador();
-        // transform.position += new Vector3(0.1f,0,0) * Time.deltaTime;
+
 
     }
 
-    public void SumarCinco(ref int numero)
-    {
-        // numero = numero + 5;
-    }
-
-    void JuntarDinero()
-    {
-        // money+=5;
-        // money-=3;
-        // Debug.Log(money);
-    }
-
-    void QuitarVidaJugador()
-    {
-        // vidaJugador -= Time.deltaTime;
-        // Debug.Log(vidaJugador);
-    }
-
-    void MovmientoJugador()
-    {
-        // transform.position += new Vector3...
-    }
-
-    void Damage()
-    {
-        // vidaPersonaje--;
-    }
 }
